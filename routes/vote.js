d55a1607b2f07209c2d950717cb05c1c4cb2119e
Spencer Scorcelletti <spencerscorcelletti@gmail.com>
2015-06-29 23:26:01 -0700

    Reenable basic voting prototype, with check that user hasn't voted on this issue before.

--- routes/vote.js
+++ routes/vote.js
@@ -1,20 +1,25 @@
 var router = require('express').Router();
 var https = require('https');
-var repos = require('../models/db').repos;
+var Issues = require('../models/db').issues;
+var passport = require('passport');
 
-router.post('/:user/:repo/:issue', function(req, res, next) {
-  model = {
-      user: req.params.user,
-      repo: req.params.repo,
-      issues : {
-        $elemMatch: { number: Number(req.params.issue) }
-      }
-  };
-
-  repos.update(model, { "$inc": { "issues.$.votes" : 1 } }, function(error, doc) {
-    if (error) throw error;
-    res.end();
-  });
+router.post('/:user/:repo/:issue', function(req, res, next) { 
+	if (req.isAuthenticated()) {
+		console.log(req.user.github.login);
+		Issues.findAndModify(
+			{ "number": Number(req.params.issue),
+				"votes" : { $nin : [req.user.github.login]} },				
+			{ $push: { "votes" : req.user.github.login }},
+			{ "new" : true })
+			.on('success', function(updated) {
+				console.log("done");
+				res.json(updated);
+			}).on('error', function(err) {
+				console.log("ERROR: " + err);
+			});				
+	} else {
+		res.json({"redirect": "/login"})
+	}
 });
 
 module.exports = router;
