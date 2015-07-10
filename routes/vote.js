97ba4c2c2c13bda3e00cbb6d931a294d52f7353f
Spencer Scorcelletti <spencerscorcelletti@gmail.com>
2015-07-09 22:23:17 -0700

    Refactor vote.js to use native mongodb driver

--- routes/vote.js
+++ routes/vote.js
@@ -1,22 +1,26 @@
 var router = require('express').Router();
 var https = require('https');
-var Issues = require('../models/db').issues;
+var Issues;
+require('../models/db')( function(err, database){
+  if (err) throw err;
+  else Issues = database.collection("issues");
+});
 var passport = require('passport');
 
-router.post('/:user/:repo/:issue', function(req, res, next) { 
+router.post('/:user/:repo/:issue', function(req, res, next) {
 	if (req.isAuthenticated()) {
 		console.log(req.user.github.login);
 		Issues.findAndModify(
 			{ "number": Number(req.params.issue),
-				"votes" : { $nin : [req.user.github.login]} },				
+				"votes" : { $nin : [req.user.github.login]} },
+			[['_id','asc']],
 			{ $push: { "votes" : req.user.github.login }},
-			{ "new" : true })
-			.on('success', function(updated) {
-				console.log("done");
-				res.json(updated);
-			}).on('error', function(err) {
-				console.log("ERROR: " + err);
-			});				
+			{ "new" : true }, function(err, document){
+				if (err) throw error;
+				else {
+					res.json(document.value);
+				}
+			});
 	} else {
 		res.json({"redirect": "/login"})
 	}
